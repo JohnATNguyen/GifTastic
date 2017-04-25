21357d67ef016d31998b2c81649c848c57468128
John Nguyen <John Nguyen>
2017-04-25 08:03:19 -0700

    4/25/17 @ 8:03am

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,13 @@ var urbanRandomQueryUrl = "https://api.urbandictionary.com/v0/random"
 var urbanDefineQueryUrl = "https://api.urbandictionary.com/v0/define?term="
 var giphySearchQueryUrl = "https://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q="
 var giphyTrendingQueryUrl = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC&limit=100"
-var numberOfAutopopulatedColumns = 3;
+var numberOfAutopopulatedColumns = 2;
 var numberOfColumns = 6;
 var gifColumnClass = `col-xl-${12/numberOfColumns} col-lg-${12/numberOfColumns} col-md-${12/numberOfColumns} col-sm-${12/numberOfColumns} col-xs-${12/numberOfColumns}`
 var gifNumber;
 var wordPhraseAltered;
 var randomTrendingGif;
+var gifsCounted = 0;
 var words = 0;
 
 function main() {
@@ -52,7 +53,8 @@ function main() {
 							$(`#${wordPhraseAltered}`).append(`<p>Rating: ${response2.data[j].rating}</p><br>`);
 						}
 					});
-					$(`#${wordPhraseAltered}`).attr('count', j);
+					$(`#${wordPhraseAltered}`).attr('count', gifsCounted);
+					gifsCounted++;
 				}
 				words++;
 			}
@@ -74,7 +76,7 @@ function random() {
 			url: urbanRandomQueryUrl,
 			method: 'GET'
 		}).done(function(response1) {
-			var	wordPhraseAltered = response1.list[0].word.replace(/ /g, '_');
+			wordPhraseAltered = response1.list[0].word.replace(/ /g, '_');
 			$('#gifs').append(`
 				<div id="${wordPhraseAltered}" class="${gifColumnClass}" value="${words}" count='0' bill="true">
 					<button class="gifButton btn btn-default">${response1.list[0].word}</button>
@@ -104,7 +106,8 @@ function random() {
 						$(`#${wordPhraseAltered}`).append(`<p>Rating: ${response2.data[j].rating}</p><br>`);
 					}
 				});
-				$(`#${wordPhraseAltered}`).attr('count', j);
+				$(`#${wordPhraseAltered}`).attr('count', gifsCounted);
+				gifsCounted++;
 			}
 			words++;
 		});
@@ -132,79 +135,102 @@ $(document).on('click', '.gif', function() {
 $("#searchbutton").on("click", function() {
     event.preventDefault();
 
-   	wordPhrase = $('#wordPhraseInput').val()
+	gifNumber = parseInt($('#numberOfGifs').val());
+   	wordPhraseAltered = $('#wordPhraseInput').val().trim().replace(/ /g, '_');
 
-    if (wordPhrase.length == 0) {
-    	$('#status').text('Please enter a word/phrase.')
-    	$('#status').show();
-    }
-    else {
-		$.ajax({
-			url: urbanDefineQueryUrl + wordPhrase.replace(/ /g, '+'),
-			method: 'GET'
-		}).done(function(response1) {
-			if (response1.list.length == 0) {
-				$('#status').text('That word/phrase is not in Urban Dictionary!');
-				$('#status').show();
-			}
-			else {
-				$('#status').hide();
-				$('#gifs').append(`
-					<div id="${words}" class="${wordPhrase} ${gifColumnClass}" count='0' bill="false">
-						<button class="gifButton btn btn-default">${wordPhrase}</button>
-						<hr>
-					</div>
-				`);
-				for (var j = 0, o = parseInt($('#numberOfGifs').val()); j < o; j++) {
-					$.ajax({
-						async: false,
-						url: giphySearchQueryUrl + wordPhrase.replace(/ /g, '+'),
-						method: 'GET'
-					}).done(function(response2) {
-						$('#' + words).append(`<img class="gif" src="${response2.data[j].images.fixed_height.url}" src-still="${response2.data[j].images.fixed_height_still.url}" src-animate="${response2.data[j].images.fixed_height.url}" state="animate" alt="Powered by Giphy">`);
-						$('#' + words).append(`<p>Rating: ${response2.data[j].rating}</p><br>`);
-					});
+	if (gifNumber >= 1 && gifNumber <= 10) {
+		if (wordPhraseAltered.length == 0) {
+	    	$('#status').text('Please enter a word/phrase.')
+	    	$('#status').show();
+	    }
+	    else {	
+			$.ajax({
+				url: urbanDefineQueryUrl + wordPhraseAltered.replace(/_/g, '+'),
+				method: 'GET'
+			}).done(function(response1) {
+				if (response1.list.length == 0) {
+					$('#status').text('That word/phrase is not in Urban Dictionary!');
+					$('#status').show();
 				}
-				words++;
-			}
-		});
+				else {
+					$('#status').hide();
+					$('#gifs').append(`
+						<div id="${wordPhraseAltered}" class="${gifColumnClass}" value="${words}" count='0' bill="true">
+							<button class="gifButton btn btn-default">${wordPhraseAltered.replace(/_/g, ' ')}</button>
+							<hr>
+						</div>
+					`);
+					for (var j = 0, o = gifNumber; j < o; j++) {
+						$.ajax({
+							async: false,
+							url: giphySearchQueryUrl + wordPhraseAltered.replace(/_/g, '+'),
+							method: 'GET'
+						}).done(function(response2) {
+							if (response2.data[j] == undefined) {
+								$(`#${wordPhraseAltered}`).append(`No (more) Giph's. Enjoy this one instead!`);
+								$.ajax({
+									async: false,
+									url: giphyTrendingQueryUrl,
+									method: 'GET'
+								}).done(function(response3) {
+									randomTrendingGif = Math.floor(Math.random() * 100);
+									$(`#${wordPhraseAltered}`).append(`<img class="gif" src="${response3.data[randomTrendingGif].images.fixed_height.url}" src-still="${response3.data[randomTrendingGif].images.fixed_height_still.url}" src-animate="${response3.data[randomTrendingGif].images.fixed_height.url}" state="animate" alt="Powered by Giphy">`);
+									$(`#${wordPhraseAltered}`).append(`<p>Rating: ${response3.data[randomTrendingGif].rating}</p><br>`);	
+								});
+							}
+							else {
+								$(`#${wordPhraseAltered}`).append(`<img class="gif" src="${response2.data[j].images.fixed_height.url}" src-still="${response2.data[j].images.fixed_height_still.url}" src-animate="${response2.data[j].images.fixed_height.url}" state="animate" alt="Powered by Giphy">`);
+								$(`#${wordPhraseAltered}`).append(`<p>Rating: ${response2.data[j].rating}</p><br>`);
+							}
+						});
+						$(`#${wordPhraseAltered}`).attr('count', gifsCounted);
+						gifsCounted++;
+					}
+					words++;
+				}
+			});
+		}
+	}
+	else {
+		$('#status').text(`Invalid number of gif's!`);
+    	$('#status').show();
 	}
 });
 
 $(document).on('click', '.gifButton', function() {
-	wordPhrase = $(this).text();
+	wordPhraseAltered = $(this).text();
 
-	$('.' + wordPhrase).html(`<button class="gifButton btn btn-default">${wordPhrase}</button><hr>`);
+	$('.' + wordPhraseAltered).html(`<button class="gifButton btn btn-default">${wordPhraseAltered}</button><hr>`);
 
 	$.ajax({
 		async: false,
-		url: giphySearchQueryUrl + wordPhrase.replace(/ /g, '+') + '&limit=100',
+		url: giphySearchQueryUrl + wordPhraseAltered.replace(/ /g, '+') + '&limit=100',
 		method: 'GET'
 	}).done(function(response2) {
-		var gifCount = parseInt($('.' + wordPhrase).attr('count'));
-		var underOneHundo = $('.' + wordPhrase).attr('bill')
+		var gifCount = parseInt($('.' + wordPhraseAltered).attr('count'));
+		var underOneHundo = $('.' + wordPhraseAltered).attr('bill')
 		if (underOneHundo) {
 			for (var j = gifCount, o = gifCount + parseInt($('#numberOfGifs').val()); j < o; j++) {
-				$('.' + wordPhrase).append(`<img class="gif" src="${response2.data[j].images.fixed_height.url}" src-still="${response2.data[j].images.fixed_height_still.url}" src-animate="${response2.data[j].images.fixed_height.url}" state="animate" alt="Powered by Giphy">`);
-				$('.' + wordPhrase).append(`<p>Rating: ${response2.data[j].rating}</p><br>`);
+				$('.' + wordPhraseAltered).append(`<img class="gif" src="${response2.data[j].images.fixed_height.url}" src-still="${response2.data[j].images.fixed_height_still.url}" src-animate="${response2.data[j].images.fixed_height.url}" state="animate" alt="Powered by Giphy">`);
+				$('.' + wordPhraseAltered).append(`<p>Rating: ${response2.data[j].rating}</p><br>`);
 				gifCount++;
 				if (gifCount >= 99) {
 					underOneHundo = false;
 				}
 			}
-			$('.' + wordPhrase).attr('count', gifCount);
-			$('.' + wordPhrase).attr('bill', underOneHundo);
-			console.log($('.' + wordPhrase).attr('bill'));
+			$('.' + wordPhraseAltered).attr('count', gifCount);
+			$('.' + wordPhraseAltered).attr('bill', underOneHundo);
+			console.log($('.' + wordPhraseAltered).attr('bill'));
 		}
 		else if (!underOneHundo) {
 			for (var i = 0, n = parseInt($('#numberOfGifs').val()); i < n; i++) {
 				gifCount = Math.floor(Math.random() * 100);
-				$('.' + wordPhrase).append(`<img class="gif" src="${response2.data[gifCount].images.fixed_height.url}" src-still="${response2.data[gifCount].images.fixed_height_still.url}" src-animate="${response2.data[gifCount].images.fixed_height.url}" state="animate" alt="Powered by Giphy">`);
-				$('.' + wordPhrase).append(`<p>Rating: ${response2.data[gifCount].rating}</p><br>`);
+				$('.' + wordPhraseAltered).append(`<img class="gif" src="${response2.data[gifCount].images.fixed_height.url}" src-still="${response2.data[gifCount].images.fixed_height_still.url}" src-animate="${response2.data[gifCount].images.fixed_height.url}" state="animate" alt="Powered by Giphy">`);
+				$('.' + wordPhraseAltered).append(`<p>Rating: ${response2.data[gifCount].rating}</p><br>`);
 			}
-			$('.' + wordPhrase).attr('count', gifCount);
-			$('.' + wordPhrase).attr('bill', underOneHundo);
+			$('.' + wordPhraseAltered).attr('count', gifCount);
+			$('.' + wordPhraseAltered).attr('bill', underOneHundo);
 		}	
 	});
-	console.log($('.' + wordPhrase).attr('count'));
+	console.log($('.' + wordPhraseAltered).attr('count'));
 });
